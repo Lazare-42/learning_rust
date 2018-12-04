76740a821ef5fd50c17ef381631979cc2c43eab5
Lazare ROSSILLON <lazrossi@e2r10p7.42.fr>
2018-12-04 09:38:40 +0100

    first steps

---
+++ hello/target/debug/deps/hello-ec1c6d4f43cb7b40.d
@@ -0,0 +1,5 @@
+/Users/lazrossi/Documents/42/rust/hello/target/debug/deps/hello-ec1c6d4f43cb7b40: src/main.rs
+
+/Users/lazrossi/Documents/42/rust/hello/target/debug/deps/hello-ec1c6d4f43cb7b40.d: src/main.rs
+
+src/main.rs:
